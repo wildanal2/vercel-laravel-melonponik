bba3c0cbb3d3dd5543659a982984bad60a5ba8a0
wildanal2 <wildanal2@gmail.com>
2025-10-27 12:10:45 +0700

    Update environment configuration for production deployment and enhance welcome page link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
 <body class="sb-nav-fixed">
     <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
         <!-- Navbar Brand-->
-        <a class="navbar-brand ps-3" href="{{ url('/') }}">PT Melonponik</a>
+        <a class="navbar-brand ps-3" href="{{ url('/') }}">PT Melonponik|{{ asset('assets/js/scripts.js') }}</a>
         <!-- Sidebar Toggle-->
         <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i
                 class="fas fa-bars"></i></button>
